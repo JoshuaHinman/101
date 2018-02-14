e2992fada148d3b771d5498ef163d489b2682aa6
Josh Hinman <joshuahinman@gmail.com>
2018-02-14 15:59:32 -0800

    revised tictactoe.rb

--- lesson6/tictactoe.rb
+++ lesson6/tictactoe.rb
@@ -1,5 +1,5 @@
 
-DIFFICULTY = 1 # 0 for random , 1 for AI
+DIFFICULTY = 0 # 0 for random , 1 for AI
 INITIAL_MARKER = ' '
 WINS_IN_MATCH = 3
 
@@ -30,25 +30,17 @@ end
 def display_board(brd)
   empty_line = ("     |" * brd[:width]).chop
   divider_line = ("-----+" * brd[:width]).chop
-
-  system('clear') || system('cls')
+  n_s = brd[:numbered_squares]
+  clear_screen
   num = 1
   (brd[:height] - 1).times do
-    if brd[:numbered_squares] == 1
-      puts numbered_line(num, brd)
-    else
-      puts empty_line
-    end
+    puts n_s ? numbered_line(num, brd) : empty_line
     puts xo_line(num, brd)
     puts empty_line
     puts divider_line
     num += brd[:width]
   end
-  if brd[:numbered_squares] == 1
-    puts numbered_line(num, brd)
-  else
-    puts empty_line
-  end
+  puts n_s ? numbered_line(num, brd) : empty_line
   puts xo_line(num, brd)
   puts empty_line
 end
@@ -78,24 +70,24 @@ def generate_win_lines(brd)
   width = brd[:width]
   (1..(brd.length - 2)).each do |i|
     x, y = index_to_xy(i, width)
-    # left to right - x,y  x+1,y  x+2,y
-    if in_bounds(x + 1, y, brd) && in_bounds(x + 2, y, brd)
-      win_lines << [i, xy_to_index(x + 1, y, width), xy_to_index(x + 2, y, width)]
-    end
-    # top to bottom - x,y  x,y+1  x,y+2
-    if in_bounds(x, y + 1, brd) && in_bounds(x, y + 2, brd)
-      win_lines << [i, xy_to_index(x, y + 1, width), xy_to_index(x, y + 2, width)]
-    end
-    # diagonal up right - x,y  x+1,y-1  x+2,y-2
-    if in_bounds(x + 1, y - 1, brd) && in_bounds(x + 2, y - 2, brd)
-      win_lines << [i, xy_to_index(x + 1, y - 1, width), xy_to_index(x + 2, y - 2, width)]
-    end
-    # diagonal down right - x,y x+1,y+1  x+2,y+2
-    if in_bounds(x + 1, y + 1, brd) && in_bounds(x + 2, y + 2, brd)
-      win_lines << [i, xy_to_index(x + 1, y + 1, width), xy_to_index(x + 2, y + 2, width)]
-    end
+    # left to right
+    verify_win_line(win_lines, brd, x, y, x + 1, y, x + 2, y)
+    # top to bottom
+    verify_win_line(win_lines, brd, x, y, x, y + 1, x, y + 2)
+    # diagonal up right
+    verify_win_line(win_lines, brd, x, y, x + 1, y - 1, x + 2, y - 2)
+    # diagonal down right
+    verify_win_line(win_lines, brd, x, y, x + 1, y + 1, x + 2, y + 2)
   end
   win_lines
+end
+
+def verify_win_line(win_lines, brd, x1, y1, x2, y2, x3, y3)
+  if in_bounds(x2, y2, brd) && in_bounds(x3, y3, brd)
+    win_lines << [xy_to_index(x1, y1, brd[:width]),
+                  xy_to_index(x2, y2, brd[:width]),
+                  xy_to_index(x3, y3, brd[:width])]
+  end
 end
 
 def index_to_xy(num, width)
@@ -131,11 +123,11 @@ def place_piece!(turn, player_marker, computer_marker, lines, brd)
   if turn == 'player'
     player_places_piece!(player_marker, brd)
   elsif turn == 'computer'
-    computer_places_piece!(computer_marker, lines, brd)
+    computer_places_piece!(player_marker, computer_marker, lines, brd)
   end
 end
 
-def player_places_piece!(marker, brd)
+def player_places_piece!(player_marker, brd)
   square = ''
   loop do
     prompt "Choose an empty square (#{joinor(empty_squares(brd))})"
@@ -146,35 +138,33 @@ def player_places_piece!(marker, brd)
       prompt "That's not an open square."
     end
   end
-  brd[square] = marker
+  brd[square] = player_marker
 end
 
-def computer_places_piece!(marker, lines, brd)
-  win, block, setup = scan_win_lines(marker, lines, brd)
+def computer_places_piece!(player_marker, computer_marker, lines, brd)
+  win = look_for_win_move(computer_marker, lines, brd)
+  block = look_for_win_move(player_marker, lines, brd)
+  setup = look_for_setup_move(computer_marker, lines, brd)
   if DIFFICULTY > 0
     if win > 0
-      brd[win] = marker
+      brd[win] = computer_marker
     elsif block > 0
-      brd[block] = marker
+      brd[block] = computer_marker
     elsif setup > 0
-      brd[setup] = marker
+      setup = 5 if brd[5] == ' ' # 5 is best setup if open
+      brd[setup] = computer_marker
     else
-      brd[empty_squares(brd).sample] = marker
+      brd[empty_squares(brd).sample] = computer_marker
     end
   else
-    brd[empty_squares(brd).sample] = marker
+    brd[empty_squares(brd).sample] = computer_marker
   end
 end
 
-def scan_win_lines(marker, lines, brd)
-  # scans for these 3 possible moves, returns results for all, 0 if not found
+def look_for_win_move(marker, lines, brd)
   win_move = 0
-  block_move = 0
-  setup_move = 0
-
   lines.each do |a|
     marker_count = 0
-    other_count = 0
     empty_count = 0
     empty_square = 0
     a.each do |i|
@@ -183,26 +173,39 @@ def scan_win_lines(marker, lines, brd)
       elsif brd[i] == ' '
         empty_count += 1
         empty_square = i
-      else
-        other_count += 1
       end
     end
     if marker_count == 2 && empty_count == 1
       win_move = empty_square
-    elsif other_count == 2 && empty_count == 1
-      block_move = empty_square
-    elsif empty_count == 2 && marker_count == 1
+    end
+  end
+  win_move
+end
+
+def look_for_setup_move(marker, lines, brd)
+  setup_move = 0
+  lines.each do |a|
+    marker_count = 0
+    empty_count = 0
+    empty_square = 0
+    a.each do |i|
+      if brd[i] == marker
+        marker_count += 1
+      elsif brd[i] == ' '
+        empty_count += 1
+        empty_square = i
+      end
+    end
+    if marker_count == 1 && empty_count == 2
       setup_move = empty_square
     end
   end
-  setup_move = 5 if brd[5] == ' ' # 5 is best setup if open
-  return win_move, block_move, setup_move
+  setup_move
 end
 
 def board_full?(brd)
   if brd.all? { |_, v| v != ' ' }
     display_board(brd)
-    prompt "Tie game!"
     true
   end
 end
@@ -211,8 +214,6 @@ def win?(brd, active, win_lines)
   win = false
   win_lines.each do |a|
     if a.all? { |s| brd[s] == brd[a[0]] } && brd[a[0]] == active[:marker]
-      display_board(brd)
-      prompt active[:win_message]
       active[:score] += 1
       win = true
       break
@@ -221,25 +222,37 @@ def win?(brd, active, win_lines)
   win
 end
 
-def won_match?(player, computer)
+def won_match_message(player, computer)
   if player == WINS_IN_MATCH
-    prompt "You win the match!"
-    true
+    prompt "You won the match!"
   elsif computer == WINS_IN_MATCH
-    prompt "Computer wins the match!"
-    true
+    prompt "The computer won the match!"
   end
 end
 
-def play_again?
+def input_y_or_n(question)
   answer = ''
+  prompt question
   loop do
-    prompt "Do you want to play again? y/n"
-    answer = gets.chomp
-    if answer.downcase == 'y'
+    answer = gets.chomp.downcase
+    if answer == 'y'
       return true
-    else
+    elsif answer == 'n'
       return false
+    else
+      prompt "Please enter y or n"
+    end
+  end
+end
+
+def input_number(question, r = (1..100))
+  prompt question
+  loop do
+    answer = gets.chomp.to_i
+    if r.include?(answer)
+      return answer
+    else
+      prompt "Please enter a number between #{r.min} and #{r.max}"
     end
   end
 end
@@ -252,20 +265,15 @@ player = { score: 0, marker: 'X', win_message: "You win!" }
 computer = { score: 0, marker: 'O', win_message: "Computer wins!" }
 display_board({ 1 => 'T', 2 => 'I', 3 => 'C',
                 4 => 'T', 5 => 'A', 6 => 'C',
-                7 => 'T', 8 => 'O', 9 => 'E', :width => 3, :height => 3, :numbered_squares => 0 })
+                7 => 'T', 8 => 'O', 9 => 'E',
+                :width => 3, :height => 3, :numbered_squares => false })
 puts ["Shall we play a game?", "Time to play!", "Let's play!"].sample.center(18)
 puts "Press Enter".center(18)
 gets
 clear_screen
 prompt "First to three wins takes the match!"
-prompt "Do you want numbered squares? y/n"
-if gets.chomp.downcase == 'y'
-  numbered_squares = 1
-else
-  numbered_squares = 0
-end
-prompt "Enter board size (3-6)"
-size = gets.chomp.to_s.to_i
+numbered_squares = input_y_or_n "Do you want numbered squares? y/n"
+size = input_number("Enter board size (3-6)", (3..6))
 clear_screen
 
 loop do
@@ -278,26 +286,29 @@ loop do
 
   loop do
     display_board(board)
-    # display output from AI scan
-    win, block, setup = scan_win_lines(player[:marker], win_lines, board)
-    puts "win move:#{win}  block move:#{block}  setup move:#{setup}"
-
     place_piece!(turn, player[:marker], computer[:marker], win_lines, board)
     sleep(0.3) if turn == 'computer'
-    break if win?(board, player, win_lines) ||
-             win?(board, computer, win_lines) ||
-             board_full?(board)
+    if win?(board, player, win_lines)
+      display_board(board)
+      prompt player[:win_message]
+      break
+    elsif win?(board, computer, win_lines)
+      display_board(board)
+      prompt computer[:win_message]
+      break
+    elsif board_full?(board)
+      prompt "Tie game!"
+      break
+    end
     turn = alternate_turn(turn)
   end
   prompt "Computer: #{computer[:score]} <<>> You: #{player[:score]}"
 
-  if won_match?(player[:score], computer[:score])
-    if !play_again?
-      break
-    else
-      player[:score] = 0
-      computer[:score] = 0
-    end
+  if player[:score] == WINS_IN_MATCH || computer[:score] == WINS_IN_MATCH
+    won_match_message(player[:score], computer[:score])
+    break if !input_y_or_n("Do you want to play again? y/n")
+    player[:score] = 0
+    computer[:score] = 0
   else
     prompt "Press Enter"
     gets
